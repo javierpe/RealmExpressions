ac986eecaddefe87eb8743110c2913c8aff1e7cd
Francisco Javier <francisco.javier.p.ramos@gmail.com>
2018-03-26 10:46:33 -0600

    Doc

--- app/src/main/java/com/oxigen/realmexpressionsproject/MainActivity.java
+++ app/src/main/java/com/oxigen/realmexpressionsproject/MainActivity.java
@@ -15,6 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Realm.init(this);
+        new RealmExpression.Builder().withTemplate("").evaluateAsync(new OnEvaluationListener() {
+            @Override
+            public void onEvaluationResult(String template, Object result) {
 
+            }
+
+            @Override
+            public void onExpressionResult(String key, String expression, Object result) {
+
+            }
+
+            @Override
+            public void onError(String key, String expression, Throwable throwable) {
+
+            }
+        });
     }
 }
